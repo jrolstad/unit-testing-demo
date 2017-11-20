a71aa5d6b18013cba0ad55a75ca990c5f5295713
Josh Rolstad <jorolsta@microsoft.com>
2017-11-20 07:25:28 -0800

    Enhancing test project

--- myservice.mvc.test/TestUtility/TestCompositionRoot.cs
+++ myservice.mvc.test/TestUtility/TestCompositionRoot.cs
@@ -1,30 +1,64 @@
 ﻿using System;
 using Microsoft.Extensions.DependencyInjection;
 using myservice.mvc.Config;
+using Microsoft.AspNetCore.Hosting.Internal;
+using Microsoft.Extensions.Configuration;
+using Moq;
 
 namespace myservice.mvc.test.TestUtility
 {
     public class TestCompositionRoot
     {
-        IServiceProvider _serviceProvider;
+        private readonly TestContext _testContext;
+        private readonly IServiceCollection _serviceCollection;
+        private IServiceProvider _serviceProvider = null;
 
-        TestCompositionRoot(IServiceCollection services)
+        private TestCompositionRoot(IServiceCollection services, TestContext testContext)
         {
-            _serviceProvider = services.BuildServiceProvider();
+            _testContext = testContext;
+            _serviceCollection = services;
         }
 
         public static TestCompositionRoot Create()
         {
             var serviceCollection = new ServiceCollection();
+            var context = new TestContext();
 
-            DependencyInjectionConfig.Configure(serviceCollection);
-
-            return new TestCompositionRoot(serviceCollection);
+            return new TestCompositionRoot(serviceCollection, context);
         }
 
         public T Get<T>() where T : new()
         {
+            if (_serviceProvider == null)
+            {
+                _serviceProvider = Build();
+            }
+
             return _serviceProvider.GetService<T>();
+        }
+
+        private IServiceProvider Build()
+        {
+            var config = new ConfigurationBuilder()
+                .AddInMemoryCollection(_testContext.ConfigurationValues)
+                .Build();
+
+            var startupInstance = new Startup(config);
+            startupInstance.ConfigureServices(_serviceCollection);
+
+
+            return _serviceCollection.BuildServiceProvider();
+        }
+
+        private Mock<T> RegisterMock<T>(IServiceCollection services) where T : class
+        {
+            var mock = new Mock<T>();
+
+            services.AddSingleton(mock);
+
+            services.AddSingleton(mock.Object);
+
+            return mock;
         }
     }
 }
---
+++ myservice.mvc.test/TestUtility/TestContext.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace myservice.mvc.test.TestUtility
+{
+    public class TestContext
+    {
+        public Dictionary<string,string> ConfigurationValues { get; set; }
+    }
+}
