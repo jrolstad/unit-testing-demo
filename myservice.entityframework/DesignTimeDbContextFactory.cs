be812f5788139dbf360dd97545df3e1309d97d49
Josh Rolstad <jorolsta@microsoft.com>
2017-11-20 07:50:52 -0800

    Adding entity framework solution

---
+++ myservice.entityframework/DesignTimeDbContextFactory.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace myservice.entityframework
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<MyServiceContext>
+    {
+        public MyServiceContext CreateDbContext(string[] args)
+        {
+            var configurationBuilder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", true);
+
+            configurationBuilder.AddJsonFile($"C:\\machineSettings.json", optional: true);
+
+            var configuration = configurationBuilder.Build();
+
+            var builder = new DbContextOptionsBuilder<MyServiceContext>();
+
+            var connectionString = configuration.GetConnectionString("MyServiceDatabase");
+
+            builder.UseSqlServer(connectionString);
+
+            return new MyServiceContext(builder.Options);
+        }
+    }
+}
---
+++ myservice.entityframework/MyServiceContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace myservice.entityframework
+{
+    public class MyServiceContext:DbContext
+    {
+        public MyServiceContext(DbContextOptions<MyServiceContext> options) : base(options)
+        {
+            // Needed for migrations
+
+            // Add-Migration -Name InitialDb -StartupProject myservice.EntityFramework -Project myservice.EntityFramework -Verbose
+
+            // Update-Database -StartUpProject myservice.EntityFramework -Project myservice.EntityFramework -Verbose
+
+            // Script-Migration -StartUpProject myservice.EntityFramework -Project myservice.EntityFramework -Verbose
+        }
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Person>(entity =>
+            {
+                entity.Property(e => e.FirstName).IsRequired().HasMaxLength(128);
+                entity.Property(e => e.LastName).IsRequired().HasMaxLength(128);
+                entity.Property(e => e.BirthDate).IsRequired();
+            });
+        }
+
+        public DbSet<Person> Person { get; set; }
+
+    }
+}
